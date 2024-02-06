4e4715753fd9ff551f7d77ac075444b1599b4028
devo_bhai <devendra.s@consultadd.com>
2024-02-06 08:14:59 -0500

    auth api completion

---
+++ myproject/myapp/serializers.py
@@ -0,0 +1,17 @@
+# myapp/serializers.py
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user

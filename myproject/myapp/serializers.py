20c48afd2aa42d1d470d4f38ad4bebffcb24bb4c
devo_bhai <devendra.s@consultadd.com>
2024-02-13 02:34:14 -0500

    resolved the email to email_address

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -2,10 +2,13 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+
 class UserSerializer(serializers.ModelSerializer):
+    # these fields are not the model fields, but the api(json from frontend) fields
+    email_address = serializers.EmailField(source='email') # this email points to customUser email
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email_address', 'password']
 
     def create(self, validated_data):
         user = CustomUser(
